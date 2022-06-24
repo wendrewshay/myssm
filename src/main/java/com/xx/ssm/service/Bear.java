a3b118c8796bc09182f8f02b746354ebf87b2d57
x_wq <x_wq@winning.com.cn>
2022-06-24 13:09:00 +0800

    测试jdk、cglib代理

---
+++ src/main/java/com/xx/ssm/service/Bear.java
@@ -0,0 +1,11 @@
+package com.xx.ssm.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bear {
+
+    public void say() {
+        System.out.println("我是一只熊");
+    }
+}
--- src/test/java/SpringTests.java
+++ src/test/java/SpringTests.java
@@ -1,5 +1,6 @@
 import com.xx.ssm.domain.User;
 import com.xx.ssm.service.A;
+import com.xx.ssm.service.Bear;
 import com.xx.ssm.service.Person;
 import com.xx.ssm.service.UserService;
 import org.junit.After;
@@ -38,13 +39,22 @@ public class SpringTests {
     }
 
     /**
-     * 测试aop
+     * 测试aop、jdk代理
      */
     @Test
     public void test3() {
         // 不仅开启aop代理中的proxy-target-class属性，且事务管理器的proxy-target-class属性设置也会影响到全局代理的创建是jdk代理还是CGLIB代理
         Person person = context.getBean(Person.class);
         person.say();
+    }
+
+    /**
+     * 测试cglib代理
+     */
+    @Test
+    public void test4() {
+        Bear bear = context.getBean(Bear.class);
+        bear.say();
     }
 
     @After
