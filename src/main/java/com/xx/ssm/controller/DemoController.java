43167d36adf29ad73a53464fd6d7f6554f20fb5f
x_wq <x_wq@winning.com.cn>
2022-07-04 13:30:35 +0800

    修改请求方式

--- src/main/java/com/xx/ssm/controller/DemoController.java
+++ src/main/java/com/xx/ssm/controller/DemoController.java
@@ -7,10 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.xx.ssm.domain.User;
 import com.xx.ssm.service.UserService;
@@ -45,9 +42,9 @@ public class DemoController {
 	/**
 	 * 查询用户
 	 */
-	@GetMapping("/user/query/{userid}")
+	@PostMapping("/user/query")
 	@ResponseBody
-	public String demo(@PathVariable(required=true) Long userid) {
+	public String demo(Long userid) {
 		User user = userService.queryUserById(userid);
 		LOG.info("userService's type is : " + userService.getClass().getName());
 		return user != null ? user.toString() : null;
